9e72e66fa57843aa95db243ac62a89f809c3ffab
phonores <55716410+phonores@users.noreply.github.com>
2020-02-08 23:12:39 -0800

    Test

--- server/controllers/testController.js
+++ server/controllers/testController.js
@@ -1,18 +1,20 @@
 const k8s = require('@kubernetes/client-node');
 
 const testController = {};
-const kc = new k8s.KubeConfig();
-kc.loadFromDefault();
-const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+// const kc = new k8s.KubeConfig();
+// kc.loadFromDefault();
+// const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 
 /**
  * testingAB - 
  */
 testController.testingAB = (req, res, next) => {
-  k8sApi.listNamespacedPod('default').then((res) => {
-    console.log(res.body.items[0].metadata.name);
-    next();
-  });
+  // k8sApi.listNamespacedPod('default').then((res) => {
+  //   console.log(res.body.items[0].metadata.name);
+    
+  // });
+  console.log("IN TESTINGAB")
+  next();
 };
 
 module.exports = testController;
