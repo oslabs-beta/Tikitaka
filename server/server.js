1c39a143943911b5ab49a7169cfb177acc080587
phonores <55716410+phonores@users.noreply.github.com>
2020-02-09 00:11:55 -0800

    Fix webpack

--- server/server.js
+++ server/server.js
@@ -1,34 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-
-
-/**
- * Setting constants
- */
-const app = express();
-const PORT = 3000;
 const testController = require('./controllers/testController');
 
-/**
- * Handling parsing request 
- */
-app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+const app = express();
 
-/**
- * Routing
- */
+const PORT = process.env.PORT || '3000';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../build/')));
+
 app.get('/testing-ab',testController.testingAB, (req,res) => {
   // res.json(res.locals.data)
   res.send({message:'hello'})
-});
-
-/**
- * Main app
- */
-app.get('/', (req, res) => { 
-  res.sendFile(path.resolve(__dirname, '../index.html'));
 });
 
 /**
@@ -54,9 +39,6 @@ app.use((err, req, res, next) => {
   res.status(errorObj.status).json(errorObj.message);
 });
 
-/**
- *  Start server
- */
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`);
   });
--- webpack.config.js
+++ webpack.config.js
@@ -3,55 +3,83 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: "./src/index.tsx",
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "build")
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  mode: process.env.NODE_ENV, 
+  devServer: {
+    // Required for Docker to work with dev server
+    host: '0.0.0.0',
+    //host: localhost,
+    port: 8080,
+    // match the output path
+    contentBase: path.resolve(__dirname, 'build'),
+    //enable HMR on the devServer
+    hot: true,
+    //match the output 'publicPath'
+    publicPath: '/',
+    // fallback to root for other urls
+    historyApiFallback: true,
+    inline: true,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+
+    // proxy is required in order to make api calls to express server while using hot-reload webpack server
+    // routes api fetch requests from localhost:8080/api/* (webpack dev server) to localhost:3000/api/* (where our Express server is running)
+    proxy: {
+      '/testing-ab': {
+        target: 'http://localhost:3000/',
+        secure: false,
+      },
     },
-    mode: 'development',
-    module: {
-        rules: [ 
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                use:{
-                    loader: "ts-loader",
-                }
-            },
-            {
-                enforce: "pre",
-                test: /\.js$/,
-                use:{
-                    loader: "source-map-loader"
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1 } },
-                    { loader: 'sass-loader', options: { sourceMap: true } },
-                ],
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-              }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./index.html"
-        }),
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
-            filename: '[name].css',
-            chunkFilename: '[id].css',
-          })
-    ],
-    devtool: "source-map",
-    resolve: {
-        extensions: [".js", ".ts", ".tsx"]
-    }
+  }, 
+  module: {
+    rules: [ 
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use:
+        {
+          loader: "ts-loader",
+        }
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        use:
+        {
+          loader: "source-map-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            MiniCssExtractPlugin.loader,
+            { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1 } },
+            { loader: 'sass-loader', options: { sourceMap: true } },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    })
+  ],
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"]
+  }
 }
