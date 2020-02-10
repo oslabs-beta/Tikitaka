859648a2ccdcca2d5377fdf8685f9ee4889d5eb1
phonores <55716410+phonores@users.noreply.github.com>
2020-02-09 23:20:55 -0800

    Added client to connect to K8s

--- server/controllers/testController.js
+++ server/controllers/testController.js
@@ -1,20 +1,24 @@
 const k8s = require('@kubernetes/client-node');
 
 const testController = {};
-// const kc = new k8s.KubeConfig();
-// kc.loadFromDefault();
-// const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+
+const kc = new k8s.KubeConfig();
+kc.loadFromCluster();
+
+const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 
 /**
  * testingAB - 
  */
 testController.testingAB = (req, res, next) => {
-  // k8sApi.listNamespacedPod('default').then((res) => {
-  //   console.log(res.body.items[0].metadata.name);
-    
-  // });
-  console.log("IN TESTINGAB")
-  next();
+  k8sApi.listNamespacedPod('default')
+    .then((res) => {
+	    res.locals.data = 'DATA ' + res.body;
+    })
+    .catch((err) => {
+      res.locals.data = 'Error ' + err;
+    });
+    next();
 };
 
 module.exports = testController;
