00eef8ed2b5f74f47c08d8ea40fe1ae14fab0d49
phonores <55716410+phonores@users.noreply.github.com>
2020-02-09 23:41:07 -0800

    testing server

--- server/controllers/testController.js
+++ server/controllers/testController.js
@@ -2,15 +2,13 @@ const k8s = require('@kubernetes/client-node');
 
 const testController = {};
 
-const kc = new k8s.KubeConfig();
-kc.loadFromCluster();
-
-const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
-
 /**
  * testingAB - 
  */
 testController.testingAB = (req, res, next) => {
+  const kc = new k8s.KubeConfig();
+  kc.loadFromCluster();
+  const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
   k8sApi.listNamespacedPod('default')
     .then((res) => {
 	    res.locals.data = 'DATA ' + res.body;
