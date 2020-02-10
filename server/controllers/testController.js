329168802b4f12f816646ceca1620b4f0d751438
phonores <55716410+phonores@users.noreply.github.com>
2020-02-10 00:16:23 -0800

    testing raw example

--- server/controllers/testController.js
+++ server/controllers/testController.js
@@ -1,4 +1,5 @@
 const k8s = require('@kubernetes/client-node');
+const request = require('request');
 
 const testController = {};
 
@@ -8,15 +9,31 @@ const testController = {};
 testController.testingAB = (req, res, next) => {
   const kc = new k8s.KubeConfig();
   kc.loadFromCluster();
-  const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
-  k8sApi.listNamespacedPod('default')
-    .then((res) => {
-	    res.locals.data = 'DATA ' + res.body;
-    })
-    .catch((err) => {
-      res.locals.data = 'Error ' + err;
-    });
-    next();
+  
+  // const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+  const opts = {};
+  kc.applyToRequest(opts);
+
+  request.get(`${kc.getCurrentCluster().server}/api/v1/namespaces/default/pods`, opts,
+    (error, response, body) => {
+      if (error) {
+        res.locals.data = 'Error ' + error;
+        console.log(`error: ${error}`);
+      }
+      if (response) {
+        console.log(`statusCode: ${response.statusCode}`);
+      }
+      res.locals.data = 'DATA ' + body;
+      console.log(`body: ${body}`);
+      next();
+  });
+  // k8sApi.listNamespacedPod('default')
+  //   .then((res) => {
+	//     res.locals.data = 'DATA ' + res.body;
+  //   })
+  //   .catch((err) => {
+  //     res.locals.data = 'Error ' + err;
+  //   });
 };
 
 module.exports = testController;
