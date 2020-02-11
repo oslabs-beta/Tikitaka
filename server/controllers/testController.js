5198d0ac86a7f9b68e49ea3c914f85efdb7ff7d1
phonores <55716410+phonores@users.noreply.github.com>
2020-02-11 10:23:13 -0800

    test

--- server/controllers/testController.js
+++ server/controllers/testController.js
@@ -14,7 +14,7 @@ testController.setup = (req, res, next) => {
   kc.applyToRequest(opts);
 
   // Request to K8s API
-  request.get(`${kc.getCurrentCluster().server}/api/v1/namespaces/default/pods`, opts,
+  request.get(`${kc.getCurrentCluster().server}/apis/apps/v1/namespaces/default/deployments/`, opts,
     (error, response, body) => {
       if (error) {
         res.locals.data = 'Error ' + error;
@@ -27,13 +27,13 @@ testController.setup = (req, res, next) => {
       const regex = /\\"/g;
       let data = JSON.parse(body.replace(regex, "'"));
 
-      // Obtaining pods names ignoring our own pod
-      let podsNames = [];
-      data.items.forEach(element => {
-        if (!(element.metadata.name.includes('tikitaka')))
-          podsNames.push(element.metadata.name);
-      });
-      res.locals.data = podsNames;
+      // // Obtaining pods names ignoring our own pod
+      // let podsNames = [];
+      // data.items.forEach(element => {
+      //   if (!(element.metadata.name.includes('tikitaka')))
+      //     podsNames.push(element.metadata.name);
+      // });
+      res.locals.data = data;
       next();
   });
 };
