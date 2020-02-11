4d774dca2732bdf825962111834ce3a0b430a4d9
phonores <55716410+phonores@users.noreply.github.com>
2020-02-11 15:39:08 -0800

    test

--- server/controllers/testController.js
+++ server/controllers/testController.js
@@ -75,7 +75,7 @@ testController.testingAB = (req, res, next) => {
       }
       // opts = JSON.stringify(opts);
 
-      const create = await client.apis['networking.istio.io'].v1alpha3.namespaces('default').virtualservices.get()
+      const create = await client.apis['networking.istio.io'].v1alpha3.namespaces('default').virtualservices.post({body:opts})
       res.locals.data = create.body;
       console.log('Create:', create);
     } catch (err) {
