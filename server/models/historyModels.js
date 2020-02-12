82b8a946b559a58a5308b3a4bb34ffe99ce7bd3a
phonores <55716410+phonores@users.noreply.github.com>
2020-02-11 16:29:57 -0800

    test

--- server/models/historyModels.js
+++ server/models/historyModels.js
@@ -1,17 +1,17 @@
-'use strict'
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+// 'use strict'
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
 
-const historySchema = new Schema({
-  podAName: { type: String, required: true },
-  podAWeight: { type: Number },
-  podAContent: { type: String },
-  podBName: { type: String, required: true },
-  podBWeight: { type: Number },
-  podBContent: { type: String },
-  createdAt: { type: Date, default: Date.now }
-});
+// const historySchema = new Schema({
+//   podAName: { type: String, required: true },
+//   podAWeight: { type: Number },
+//   podAContent: { type: String },
+//   podBName: { type: String, required: true },
+//   podBWeight: { type: Number },
+//   podBContent: { type: String },
+//   createdAt: { type: Date, default: Date.now }
+// });
 
-const History = mongoose.model('History', historySchema);
+// const History = mongoose.model('History', historySchema);
 
-module.exports = { History };
+// module.exports = { History };
