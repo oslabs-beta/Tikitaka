c681972a80325c89b5a32161e11074567da7f8ae
Ryan Kim <tkdryan@Ryans-MacBook-Pro-10.local>
2020-02-12 17:42:20 -0800

    completed, populating deployments from kubernetes to dropdown

--- src/components/TestForm.tsx
+++ src/components/TestForm.tsx
@@ -1,88 +1,124 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import Form from 'react-bootstrap/Form'
 import { Col, Row } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button'
 import InputGroup from 'react-bootstrap/InputGroup'
-
 interface Iprops{
-
-}
-
-export default class TestForm extends React.Component<Iprops, any>{
-    const deployments = ['deployment1Name', 'deployment2Name', 'deployment3Name', 'deployment4Name'];
-
-    public render(): JSX.Element{
-        return(
-            <Form>
-                <Form.Row>
-                    <Form.Group as={Col} controlId="formGridState">
-                        <Form.Label><h4>Docker Image A:</h4></Form.Label>
-                        <Form.Control as="select">
-                            <option>Select image name...</option>
-                            <option>tikitaka-arman-image</option>
-                            <option>tikitaka-cat-image</option>
-                            <option>tikitaka-pati-image</option>
-                            <option>tikitaka-ryan-image</option>
-                            <option>tikitaka-yunho-image</option>
-                        </Form.Control>
-                        <br />
-                        <InputGroup className="mb-3">
-                            <Form.Control
-                            placeholder="Canary weight (integer between 0-100)"
-                            aria-label="Amount (to the nearest number)"
-                            />
-                            <InputGroup.Append>
-                            <InputGroup.Text>%</InputGroup.Text>
-                            <InputGroup.Text>weight</InputGroup.Text>
-                            </InputGroup.Append>
-                        </InputGroup>
-                    </Form.Group>
-
-                    <Form.Group as={Col} controlId="formGridState">
-                        <Form.Label><h4>Docker Image B: </h4></Form.Label>
-                        <InputGroup className="mb-3">
-                            <InputGroup.Prepend>
-                            <InputGroup.Text id="inputGroup-sizing-default">Name: </InputGroup.Text>
-                            </InputGroup.Prepend>
-                            <Form.Control
-                            aria-label="Default"
-                            aria-describedby="inputGroup-sizing-default"
-                            />
-                        </InputGroup>
-                        <br />
-                        <InputGroup className="mb-3">
-                            <InputGroup.Prepend>
-                            <InputGroup.Text id="inputGroup-sizing-default">Address: </InputGroup.Text>
-                            </InputGroup.Prepend>
-                            <Form.Control
-                            placeholder="Ex: 77af4d6b9913"
-                            aria-label="Default"
-                            aria-describedby="inputGroup-sizing-default"
-                            />
-                        </InputGroup>
-                        <br />
-                        <InputGroup className="mb-3">
-                            <InputGroup.Prepend>
-                            <InputGroup.Text id="inputGroup-sizing-default">Version: </InputGroup.Text>
-                            </InputGroup.Prepend>
-                            <Form.Control
-                            placeholder="1.0.0"
-                            aria-label="Default"
-                            aria-describedby="inputGroup-sizing-default"
-                            />
-                        </InputGroup>
-                        <br />
-
-                    </Form.Group>
-
-                </Form.Row>
-                <Button variant="primary" type="submit" id="createTestBtn">
-                    Create A/B Test
-                </Button>
-                <Button variant="primary">
-                    Open Kiali
-                </Button>
-            </Form>
-        )
+};
+const TestForm: React.FC<Iprops> = () => {
+    interface eachMetadataType {
+        name: string;
+        namespace: string;
+        selfLink: string;
+        uid: string;
+        resourceVersion: string;
+        generation: number;
+        creationTimestamp: string;
     }
+    interface eachItemType {
+        metadata: eachMetadataType;
+        spec: object;
+        status: object;
+    }
+    interface containersType {
+        kind: string;
+        apiVersion: string;
+        metadata: object;
+        items: eachItemType[];
+    }
+    const [containers, setContainers] = useState<containersType>({
+        kind: '',
+        apiVersion: '',
+        metadata: {},
+        items: [{metadata: {name: "api-gateway",
+        namespace: "default",
+        selfLink: "/apis/apps/v1/namespaces/default/deployments/api-gateway",
+        uid: "2ad6806d-0071-45cc-929f-09da9d21a9c9",
+        resourceVersion: "116386",
+        generation: 1,
+        creationTimestamp: "2020-02-10T20:01:48Z"}, spec: {}, status: {}}]
+    });
+    useEffect(() => {
+        const getContainers = async () => {
+            let r = await fetch('http://localhost:8081/apis/apps/v1/namespaces/default/deployments/');
+            let containers = await r.json();
+            setContainers(containers);
+        };
+        getContainers();
+    },[]);
+    const dropDown = [];
+    if (containers.items.length > 1) {
+        for(let i = 0; i < containers.items.length; i++) {
+            let list = <option id={`${i}`}>{containers.items[i].metadata.name}</option>
+            dropDown.push(list)
+        }
+    } else {
+        dropDown.push(<option id='ryan'>tikitaka-ryan-image</option>);
+    }
+    return (
+        <Form>
+            <Form.Row>
+                <Form.Group as={Col} controlId="formGridState">
+                    <Form.Label><h4>Docker Image A:</h4></Form.Label>
+                    <Form.Control as="select">
+                        {dropDown}
+                    </Form.Control>
+                    <br />
+                    <InputGroup className="mb-3">
+                        <Form.Control
+                        placeholder="Canary weight (integer between 0-100)"
+                        aria-label="Amount (to the nearest number)"
+                        />
+                        <InputGroup.Append>
+                        <InputGroup.Text>%</InputGroup.Text>
+                        <InputGroup.Text>weight</InputGroup.Text>
+                        </InputGroup.Append>
+                    </InputGroup>
+                </Form.Group>
+                <Form.Group as={Col} controlId="formGridState">
+                    <Form.Label><h4>Docker Image B: </h4></Form.Label>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Prepend>
+                        <InputGroup.Text id="inputGroup-sizing-default">Name: </InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <Form.Control
+                        aria-label="Default"
+                        aria-describedby="inputGroup-sizing-default"
+                        />
+                    </InputGroup>
+                    <br />
+                    <InputGroup className="mb-3">
+                        <InputGroup.Prepend>
+                        <InputGroup.Text id="inputGroup-sizing-default">Address: </InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <Form.Control
+                        placeholder="Ex: 77af4d6b9913"
+                        aria-label="Default"
+                        aria-describedby="inputGroup-sizing-default"
+                        />
+                    </InputGroup>
+                    <br />
+                    <InputGroup className="mb-3">
+                        <InputGroup.Prepend>
+                        <InputGroup.Text id="inputGroup-sizing-default">Version: </InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <Form.Control
+                        placeholder="1.0.0"
+                        aria-label="Default"
+                        aria-describedby="inputGroup-sizing-default"
+                        />
+                    </InputGroup>
+                    <br />
+                </Form.Group>
+            </Form.Row>
+            <Button variant="primary" type="submit" id="createTestBtn">
+                Create A/B Test
+            </Button>
+            <Button variant="primary">
+                Open Kiali
+            </Button>
+        </Form>
+    );
 }
+export default TestForm;
