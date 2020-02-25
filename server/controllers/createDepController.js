b466bafaa2e4e25f4970bbf7651aafe5afa0549b
Arman Gurkan <arman@secelektrik.com>
2020-02-24 19:49:22 -0800

    added all the func

--- server/controllers/createDepController.js
+++ server/controllers/createDepController.js
@@ -18,73 +18,59 @@ const cert = 'MIICyDCCAbCgAwIBAgIBADANBgkqhkiG9w0BAQsFADAVMRMwEQYDVQQDEwprdWJl\n
     'szYyd3UnItjzrHVXVONpODv149Z8wfnytRbgv02N7sHGhLJ38Jh0r+Hd3zk=';
 const token = fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token', 'utf8');
 const body = {
-  "apiVersion": "networking.istio.io/v1alpha3",
-  "kind": "VirtualService",
+  "kind": "Deployment",
   "metadata": {
-    "annotations": {},
-    "name": "gotlek-cluster-ip-service",
-    "namespace": "default"
+    "name": "tiki-taka-rocks-hard",
+    "labels": {
+      "app": "reviews",
+      "version": "v1"
+    }
   },
   "spec": {
-    "gateways": [
-      "ingress-gateway-configuration"
-    ],
-    "hosts": [
-      "*"
-    ],
-    "http": [
-      {
-        "match": [
-          {
-            "uri": {
-              "prefix": "/topitop"
-            }
-          }
-        ],
-        "route": [
-          {
-            "destination": {
-              "host": "arman-cluster-ip-service"
-            }
-          }
-        ]
+    "replicas": 1,
+    "selector": {
+      "matchLabels": {
+        "app": "reviews",
+        "version": "v1"
+      }
+    },
+    "template": {
+      "metadata": {
+        "labels": {
+          "app": "reviews",
+          "version": "v1"
+        }
       },
-      {
-        "match": [
+      "spec": {
+        "serviceAccountName": "bookinfo-reviews",
+        "containers": [
           {
-            "uri": {
-              "prefix": "/taksi"
-            }
-          }
-        ],
-        "route": [
-          {
-            "destination": {
-              "host": "client-cluster-ip-service",
-              "subset": "original"
-            },
-            "weight": 90
-          },
-          {
-            "destination": {
-              "host": "client-cluster-ip-service",
-              "subset": "experimental"
-            },
-            "weight": 10
+            "name": "reviews",
+            "image": "docker.io/istio/examples-bookinfo-reviews-v1:1.15.0",
+            "imagePullPolicy": "IfNotPresent",
+            "ports": [
+              {
+                "containerPort": 9080
+              }
+            ]
           }
         ]
       }
-    ]
+    }
   }
 };
 
 const createDepController = {};
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 createDepController.addVirtualService = (req, res, next) => {
+  let newIm = {};
+  const { imageA, weightA, imageB, addressB, versionB } = req.body;
+  fetch(`https://kubernetes.default.svc/apis/apps/v1/namespaces/default/deployments/reviews-v1`)
+  .then(data => data.json()).then(json => {newIm = json}).catch(e => console.log(e));
   
-  fetch('https://kubernetes.default.svc/apis/networking.istio.io/v1alpha3/namespaces/default/virtualservices/', {
+  fetch('https://kubernetes.default.svc/apis/apps/v1/namespaces/default/deployments/', {
     method: 'POST',
-    body: b,
+    body: JSON.stringify(body),
     requestCert: false,
     rejectUnauthorized: false,
     headers: { 'Content-Type': 'application/json',
